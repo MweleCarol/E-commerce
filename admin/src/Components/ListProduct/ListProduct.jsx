23a75f124026fe43f7dbd4337627cbbaf0a24aaa
MweleCarol <mwelecarolyne65@gmail.com>
2025-10-21 10:11:19 +0300

    Done with Login and Signup

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -15,6 +15,17 @@ const ListProduct = () => {
     fetchInfo();
   },[])
 
+  const remove_product = async (id)=>{
+    await fetch ('http://localhost:4000/removeproduct',{
+      method:'POST',
+      headers:{
+        Accept:'application/json',
+        'Content-Type':'application/json',
+      },
+      body:JSON.stringify({id:id})
+    })
+    await fetchInfo();
+  }
   return (
     <div className='list-product'>
       <h1>All Products List</h1>
@@ -35,7 +46,7 @@ const ListProduct = () => {
               <p>${product.old_price}</p>
               <p>${product.new_price}</p>
               <p>{product.category}</p>
-              <img src={cross_icon} alt="" className="listproduct-remove-icon" />
+              <img onClick={()=>{remove_product(product.id)}} src={cross_icon} alt="" className="listproduct-remove-icon" />
           </div>
         })}
       </div>
